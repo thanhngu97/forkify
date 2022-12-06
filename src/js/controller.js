7bb633dbb2a49f9245e5b0d0a244879fa71a00cc
Ngu Thanh Duong <thanhngu2110@gmail.com>
2022-12-06 10:06:38 +0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -102,6 +102,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the Forkify application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -110,5 +114,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
