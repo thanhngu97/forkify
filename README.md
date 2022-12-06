aee75bee06128fe71a60d9556b993e6627a77717
Ngu Thanh Duong <thanhngu2110@gmail.com>
2022-12-06 09:53:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# forkify Project
+
+Recipe app with custom recipe upload
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = '0fe94a43-114b-49e9-986b-75b7d4444ef7';
+export const MODAL_CLOSE_TIME = 1.5;
